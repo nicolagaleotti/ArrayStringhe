d6d94e02b94aa4b8cfa056bdd1ca70d4fdd0e9d5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-08 12:33:27 +0100

    fine

--- Array/MainWindow.xaml.cs
+++ Array/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Array
 {
@@ -23,6 +24,39 @@ namespace Array
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+            string[] array = new string[5];
+            int i = 0;
+            string file = @"array.txt";
+            string testo;
+        private void btnInserisci_Click(object sender, RoutedEventArgs e)
+        {
+            string text = txtStringa.Text;
+            array[i] = text;
+            i++;
+            txtStringa.Clear();
+            txtStringa.Focus();
+            if (i == 5)
+                btnInserisci.IsEnabled = false;
+        }
+        
+        private void btnStampa_Click(object sender, RoutedEventArgs e)
+        {
+            for (int c = 0; c < array.Length; c++)
+            {
+                testo += $"Posizione {c+1} : {array[c]} \n";
+            }
+            lblResult.Content = testo;
+        }
+
+        private void btnPubblica_Click(object sender, RoutedEventArgs e)
+        {
+            
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                writer.Write(lblResult.Content);
+            }
         }
     }
 }
